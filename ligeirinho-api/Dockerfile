3fd638f319413b9205faca0a1ee9f5c10fa1a008
Catharina Mesquita <catha.ana.1994@gmail.com>
2023-04-10 11:38:31 -0300

    feat: add Docker and Sidekiq

---
+++ ligeirinho-api/Dockerfile
@@ -0,0 +1,9 @@
+FROM ruby:3.2.1
+
+WORKDIR /app
+
+COPY . .
+
+RUN bundle install
+
+EXPOSE 3000
--- ligeirinho-api/config/routes.rb
+++ ligeirinho-api/config/routes.rb
@@ -1,4 +1,13 @@
+require 'sidekiq/web'
+
+# Configure Sidekiq-specific session middleware
+Sidekiq::Web.use ActionDispatch::Cookies
+Sidekiq::Web.use ActionDispatch::Session::CookieStore, key: "_interslice_session"
+
 Rails.application.routes.draw do
+
+  mount Sidekiq::Web => '/sidekiq'
+
   post '/customers', to: 'customers#create'
   post '/customers/:customer_id/orders', to: 'orders#create'
   put '/customers/:customer_id/orders/:order_id/init_delivery', to: 'orders#init_delivery'
